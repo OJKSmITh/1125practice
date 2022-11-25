09bbb36540c89ceb695cef425553770214c9fb60
char1ey <ckstn410@gmail.com>
2022-11-25 20:10:52 +0900

    feat: list.js 형변환 및 DOM 기능 추가

--- public/js/list.js
+++ public/js/list.js
@@ -8,4 +8,21 @@ if(item === null) {
 }
 
 const tbody = document.querySelector("tbody")
-console.log(tbody)
+
+function template(item, index){
+    return`
+    <tr>
+        <td>${index + 1}</td>
+        <td><a href="/board/view.html?index=${index}">${item.subject}</a></td>
+        <td>${item.content}</td>
+        <td>${item.date}</td>
+        <td>${item.hit}</td>
+    </tr>
+    `
+}
+
+const boards = JSON.parse(item)
+
+for(let i = boards.length - 1; i >= 0; i--){
+    tbody.innerHTML += template(boards[i], i)
+}
