798b1f21220c6c85b301312719468c37d0e40496
char1ey <ckstn410@gmail.com>
2022-11-25 19:56:35 +0900

    feat: list.js 초기 데이터 생성 기능 추가

--- public/js/list.js
+++ public/js/list.js
@@ -1,1 +1,8 @@
 let item = localStorage.getItem("boards")
+
+if(item === null) {
+    const intialState = []
+    const state = JSON.stringify(intialState)
+    localStorage.setItem("boards", state)
+    item = state
+}
